98fad67583c74efc0e59bbee5558a718e4cf144e
sulimanbenhalim <soliman.benhalim@gmail.com>
2026-02-03 19:58:25 +0200

    Fix Homebrew formula to install dependencies correctly

--- Formula/ga-cli.rb
+++ Formula/ga-cli.rb
@@ -3,18 +3,18 @@ class GaCli < Formula
 
   desc "Command-line interface for Google Analytics 4"
   homepage "https://github.com/sulimanbenhalim/ga-cli"
-  url "https://files.pythonhosted.org/packages/dd/55/fe60133f220ff31795e00046e7a8cd868334af17ec0bc37b0dc0083e7536/ga4_cli-0.1.0.tar.gz"
-  sha256 "a7a9d60a92487050ed405776f688c6841cd184c5e5af0d95459b91c5125d8495"
+  url "https://files.pythonhosted.org/packages/8e/b1/30cb8b086c9b58920afbb5915a13108371b69bd8ec64f8ce0c9cdd217ec4/ga4_cli-0.1.1.tar.gz"
+  sha256 "3849da0726068cc2c6c130d0e174385d54c06b19296c26383b05177766c5f20d"
   license "MIT"
 
   depends_on "python@3.11"
 
   def install
-    virtualenv_install_with_resources
+    virtualenv_create(libexec, "python3.11")
+    system libexec/"bin/pip", "install", buildpath
   end
 
   test do
     system bin/"ga-cli", "--version"
-    assert_match "0.1.0", shell_output("#{bin}/ga-cli --version")
   end
 end
